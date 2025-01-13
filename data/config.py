17be882455b783929659d4e1badabde8764f3af4
cpuQ <102803247+phruut@users.noreply.github.com>
2025-01-13 13:56:06 +0900

    idk

--- data/config.py
+++ data/config.py
@@ -1,8 +1,11 @@
-# ill set up a proper one later
+from collections import namedtuple
+
+# this is so cool
+point = namedtuple('point', ['x', 'y'])
 
 class Config:
     def __init__(self):
-        self.VERSION = 0.4
+        self.VERSION = 0.5
         self.RUNNING = False
         self.HWND = None
         self.WINTITLE = 'Roblox'
@@ -17,37 +20,35 @@ class Config:
         self.BACKWARD = 's'
 
         # text coords
-        self.TEXT_X = 404
-        self.TEXT_Y = 426
+        self.TEXT_POS = point(404, 426)
         self.POINT_COL = (255, 255, 1)
 
         # game text coords
-        self.RLGL_GAME_X = 315
-        self.RLGL_GAME_Y = 426
+        self.RLGL_GAME_POS = point(315, 426)
         self.GAME_COL = (253, 48, 145)
 
         # lobby indicator (menu icon)
-        self.LOBBY_X = 420
-        self.LOBBY_Y = 535
+        self.LOBBY_POS = point(420, 535)
         self.LOBBY_COL = (210, 235, 35)
 
         # home icon coords
-        self.HOME_X = 404
-        self.HOME_Y = 516
+        self.HOME_POS = point(404, 516)
 
         # eliminated
-        self.ELIM_X = 550
-        self.ELIM_Y = 560
+        self.ELIM_POS = point(550, 560)
         self.ELIM_COL = (255, 9, 74)
 
         # rlgl colors
-        self.RLGL_X = 396
-        self.RLGL_Y = 128
+        self.RLGL_POS = point(396, 128)
         self.RLGL_RED = (255, 24, 24)
         self.RLGL_GREEN = (83, 255, 56)
         self.RLGL_COL = (230, 235, 157)
 
+        # rlgl orientation check
+        self.RLGL_ORI_POS1 = point(150, 300)
+        self.RLGL_ORI_POS2 = point(650, 300)
+        self.RLGL_ORI_COL = (255, 0, 0)
+
         # obby color
-        self.OBBY_X = 790
-        self.OBBY_Y = 590
+        self.OBBY_POS = point(790, 590)
         self.OBBY_COL = (83, 154, 195)
