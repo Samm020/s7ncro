07d998b1db252bfeb7bb7bdd98ceea2c880730d5
cpuQ <102803247+phruut@users.noreply.github.com>
2025-01-14 12:33:06 +0900

    error handling

--- utils/_sm.py
+++ utils/_sm.py
@@ -8,11 +8,11 @@ class StatusManager:
     def update(self, target, value):
         """update the status text thing yeah"""
         with self.status_lock:
-            if dpg.is_dearpygui_running():
+            if dpg.is_dearpygui_running() and dpg.does_item_exist(target):
                 dpg.set_value(target, value)
 
     def configure(self, target, **kwargs):
         """configure dpg thing"""
         with self.status_lock:
-            if dpg.is_dearpygui_running():
+            if dpg.is_dearpygui_running() and dpg.does_item_exist(target):
                 dpg.configure_item(target, **kwargs)
