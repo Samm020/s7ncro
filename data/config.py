91e65d8c42ac0746efd417ab53c5e6605aff187f
cpuQ <102803247+phruut@users.noreply.github.com>
2025-01-14 17:09:52 +0900

    version

--- data/config.py
+++ data/config.py
@@ -5,7 +5,7 @@ point = namedtuple('point', ['x', 'y'])
 
 class Config:
     def __init__(self):
-        self.VERSION = 0.8
+        self.VERSION = 0.9
         self.RUNNING = False
         self.HWND = None
         self.WINTITLE = 'Roblox'
